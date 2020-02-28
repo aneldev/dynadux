5e0a0a365f7102427fbf8711995985f414ce7369
dennisat <dennis@anel.co>
2020-02-28 05:16:12 +0100

    impr: export from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export * from "./Dynadux/Dynadux";
-export * from "./createStore/createStore";
-export * from "./middlewares/dynaduxDebugMiddleware";
+export {
+  Dynadux,
+  IDynaduxConfig,
+  IDynaduxReducerAPI,
+  IDynaduxMiddleware,
+  IDynaduxMiddlewareBeforeAPI,
+  IDynaduxMiddlewareAfterAPI,
+  TDynaduxDispatch,
+  TDynaduxReducer,
+} from "./Dynadux/Dynadux";
+
+export {
+  createStore,
+  ICreateStoreConfig,
+  ICreateStoreAPI,
+} from "./createStore/createStore";
+
+export {
+  dynaduxDebugMiddleware,
+} from "./middlewares/dynaduxDebugMiddleware";
