f4eed826e995ada41dba5abc3ac34a77c5c3813b
dennisat <dennis@anel.co>
2020-03-01 18:25:52 +0100

    impr: dynaduxDebugMiddleware with IDynaduxDebugMiddlewareConfig

--- src/middlewares/dynaduxDebugMiddleware.ts
+++ src/middlewares/dynaduxDebugMiddleware.ts
@@ -1,11 +1,18 @@
 import {IDynaduxMiddleware} from "../Dynadux/Dynadux";
 import {global}  from "../tools/global";
 
-export const dynaduxDebugMiddleware = (): IDynaduxMiddleware => {
+export interface IDynaduxDebugMiddlewareConfig {
+  globalVariableName?: string;
+}
 
+export const dynaduxDebugMiddleware = (
+  {
+    globalVariableName = 'dynaduxDebugMiddleware',
+  }: IDynaduxDebugMiddlewareConfig = {}
+): IDynaduxMiddleware => {
   let lastDispatch = 0;
   let dispatchNo = 0;
-  global.dynaduxDebugMiddleware = [];
+  global[globalVariableName] = [];
 
   return {
     after: (
@@ -19,13 +26,21 @@ export const dynaduxDebugMiddleware = (): IDynaduxMiddleware => {
       const date = new Date;
       dispatchNo++;
 
-      global.dynaduxDebugMiddleware.push({
-        desc: `#${(dispatchNo)} ${action} ${date.toTimeString()}`,
+      const afterMs = (() => {
+        if (lastDispatch === 0) return undefined;
+        return date.valueOf() - lastDispatch;
+      })();
+
+      global[globalVariableName].push({
+        desc:
+          [
+            frontSpace(' ', '#' + dispatchNo, 5),
+            frontSpace(' ', `+${duration(afterMs)}`, 9),
+            action,
+            date.toTimeString()
+          ].join(' '),
         action,
-        afterMs: (() => {
-          if (lastDispatch === 0) return undefined;
-          return date.valueOf() - lastDispatch;
-        })(),
+        afterMs,
         payload,
         before: initialState,
         after: state,
@@ -37,3 +52,13 @@ export const dynaduxDebugMiddleware = (): IDynaduxMiddleware => {
   };
 };
 
+const frontSpace = (spacer: string, content: string | number, length: number): string => {
+  const text = String(content);
+  if (text.length >= length) return text;
+  return `${Array(length).fill(spacer).join('')}${text}`.substr(-length);
+};
+
+const duration = (d: number = 0): string => {
+  if (d > 10000) return (d / 1000).toFixed(2) + 'sec';
+  return d + 'ms';
+};
