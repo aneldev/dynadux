0bb8d78747d5cc2643390f326d9e1790b67efba9
dennisat <dennis@anel.co>
2020-04-24 15:15:40 +0200

    Build

--- dist/esNext/index.d.ts
+++ dist/esNext/index.d.ts
@@ -1,4 +1,3 @@
 export { Dynadux, IDynaduxConfig, IDynaduxReducerDic, IDynaduxReducerAPI, IDynaduxMiddleware, IDynaduxMiddlewareBeforeAPI, IDynaduxMiddlewareAfterAPI, TDynaduxReducerDispatch as TDynaduxDispatch, TDynaduxReducerDispatch, TDynaduxMiddlewareDispatch, TDynaduxReducer, } from "./Dynadux/Dynadux";
 export { createStore, ICreateStoreConfig, ICreateStoreAPI, } from "./create/createStore";
-export { createSection, } from "./create/createSection";
 export { dynaduxDebugMiddleware, IDebugLogItem, } from "./middlewares/dynaduxDebugMiddleware";
--- dist/esNext/index.js
+++ dist/esNext/index.js
@@ -1,5 +1,4 @@
 export { Dynadux, } from "./Dynadux/Dynadux";
 export { createStore, } from "./create/createStore";
-export { createSection, } from "./create/createSection";
 export { dynaduxDebugMiddleware, } from "./middlewares/dynaduxDebugMiddleware";
 //# sourceMappingURL=index.js.map
